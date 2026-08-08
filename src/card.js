9d339e365f9fe9e80e1da01fe1534618a57bcf77
ekansh-debug <ekansh123123@gmail.com>
2026-08-08 20:26:58 +0530

    added card . as the todo card

---
+++ src/card.js
@@ -0,0 +1,28 @@
+export default function card(titel = "Add titel" , info = "Add Info", isChecked = false) {
+    const box = document.createElement("div");
+    box.classList.add("");
+
+    const cardTitel = document.createElement("h2");
+    cardTitel.classList.add("todo-title");
+    cardTitel.textContent = titel;
+
+    const cardInfo = document.createElement("div");
+    cardInfo.classList.add("todo-info");
+    cardInfo.textContent = info;
+
+    const cardCheckBox = document.createElement("input");
+    cardCheckBox.classList.add("todo-checkbox");
+    cardCheckBox.type = "checkbox";
+    cardCheckBox.checked = isChecked;
+
+    cardCheckBox.addEventListener("click",() =>{
+        box.classList.toggle("completed", cardCheckBox.checked);
+    });
+
+
+    box.append(cardTitel);
+    box.append(cardInfo);
+    box.append(cardCheckBox);
+
+    return box;
+}
