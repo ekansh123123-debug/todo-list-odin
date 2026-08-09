9c13b245a706ec162fea0bdb235af6a26dedc6db
ekansh-debug <ekansh123123@gmail.com>
2026-08-09 10:41:44 +0530

    made it follow SOLID principle

--- src/card.js
+++ src/card.js
@@ -1,15 +1,24 @@
-export default function card(titel = "Add titel" , info = "Add Info", isChecked = false) {
+const createBox = () => {
     const box = document.createElement("div");
     box.classList.add("todo-card");
+    return box;
+}
 
-    const cardTitel = document.createElement("h2");
-    cardTitel.classList.add("todo-title");
-    cardTitel.textContent = titel;
+const createTitle = (title) =>{
+    const cardTitle = document.createElement("h2");
+    cardTitle.classList.add("todo-title");
+    cardTitle.textContent = title;
+    return cardTitle;
+}
 
+const createInfo = (info) => {
     const cardInfo = document.createElement("div");
     cardInfo.classList.add("todo-info");
     cardInfo.textContent = info;
+    return cardInfo;
+}
 
+const createCheckbox = (isChecked) =>{
     const cardCheckBox = document.createElement("input");
     cardCheckBox.classList.add("todo-checkbox");
     cardCheckBox.type = "checkbox";
@@ -18,8 +27,16 @@ export default function card(titel = "Add titel" , info = "Add Info", isChecked 
     cardCheckBox.addEventListener("click",() =>{
         box.classList.toggle("completed", cardCheckBox.checked);
     });
+    return cardCheckBox;
+}
 
-    box.append(cardTitel);
+export default function card(title = "Add title" , info = "Add Info", isChecked = false) {
+    const box = createBox();
+    const cardTitle = createTitle(title);
+    const cardInfo = createInfo(info);
+    const cardCheckBox = createCheckbox(isChecked);
+
+    box.append(cardTitle);
     box.append(cardInfo);
     box.append(cardCheckBox);
 
