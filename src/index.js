306259601cfd67374abac06b1116044c7476ad8f
ekansh-debug <ekansh123123@gmail.com>
2026-08-13 19:07:35 +0530

    modified index.js to accomidate this changes in different files

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-import "./style.css"
-import card from "./card.js"
-import homePage from "./homePage.js"
-import cardManage from "./cardManage.js"
+import "./style.css";
+import homePage from "./createHomePage.js";
+import dialogManage from "./dialogMange.js";
 
 let cards = [];
 
 const elements = {
-    'addTaskBtn' : document.querySelector("#addBtn"),
-    'homeBtn' : document.querySelector("#homeBtn"),
-    'completBtn' : document.querySelector("#completTask"),
-    'dialog' : document.querySelector("#dialog"),
-    'todoForm' : document.querySelector("#todoForm"),
-    'saveBtn' : document.querySelector("#saveBtn"),
-    'cancleBtn' : document.querySelector("#cancelBtn"),
-    'todoContent' : document.querySelector("#todoContant")
+    'addTaskBtn': document.querySelector("#addBtn"),
+    'homeBtn': document.querySelector("#homeBtn"),
+    'completeBtn': document.querySelector("#completTask"),
+    'saveBtn': document.querySelector("#saveBtn"),
+    'cancelBtn': document.querySelector("#cancelBtn"),
+    'dialog': document.querySelector("#dialog"),
+    'todoForm': document.querySelector("#todoForm"),
+    'todoContent': document.querySelector("#todoContant")
 };
 
-elements.addTaskBtn.addEventListener("click",() => elements.dialog.showModal());
-elements.saveBtn.addEventListener("click" , () => {
-    const cardData = {
-        "title": document.querySelector("#title").value,
-        "info" : document.querySelector("#info").value,
-        "inputDate" : document.querySelector("#dueDate").value,
-        "initialDate" : new Date(),
-        "isChecked" : false,
-    }
+elements.homeBtn.addEventListener("click", () => homePage(elements, cards));
+elements.addTaskBtn.addEventListener("click", () => elements.dialog.showModal());
 
-    elements.todoContent.append(card(cardData));
-    cards.push(cardData);
-
-    elements.todoForm.reset();
-    elements.dialog.close();
-})
-elements.cancleBtn.addEventListener("click", () => elements.dialog.close());
-
+elements.saveBtn.addEventListener("click", () => dialogManage(elements, cards));
+elements.cancelBtn.addEventListener("click", () => elements.dialog.close());
