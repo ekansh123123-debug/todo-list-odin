c20de9c2793493964bf2382dd93645301c1302ec
ekansh-debug <ekansh123123@gmail.com>
2026-08-13 19:06:50 +0530

    create a dialogManage.js to mange the dialog interface

---
+++ src/dialogMange.js
@@ -0,0 +1,19 @@
+import card from "./createCard.js"
+import homePage from "./createHomePage.js";
+
+export default function (elements, cards) {
+    const cardData = {
+        "ID" : crypto.randomUUID(),
+        "title": document.querySelector("#title").value,
+        "info": document.querySelector("#info").value,
+        "inputDate": new Date(document.querySelector("#dueDate").value),
+        "initialDate": new Date(),
+        "isChecked": false
+    }
+
+    cards.push(cardData);
+
+    elements.todoForm.reset();
+    elements.dialog.close();
+    homePage(elements, card);
+}
