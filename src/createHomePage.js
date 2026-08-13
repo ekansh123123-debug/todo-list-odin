fafcc9ef22dd6d561d668391c91a348e8baa754c
ekansh-debug <ekansh123123@gmail.com>
2026-08-13 22:21:02 +0530

    changed createHomePage to make it append every thing in the cards to todoContent

--- src/createHomePage.js
+++ src/createHomePage.js
@@ -1,7 +1,8 @@
 import card from "./createCard.js"
 
 export default function homePage(todoContent, cards) {
-    cards.foreach((currentCard) => {
-        if(currentCard.isChecked) todoContent.append(card(currentCard,cards));
+    cards
+      .forEach((checkedCard) => {
+        todoContent.append(card(checkedCard, cards));
     });
 }
